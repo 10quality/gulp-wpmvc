b575316063f2020db8c098ef9fdb9a31bcdbfbee
Alejandro Mostajo <amostajo@gmail.com>
2017-06-30 20:31:55 -0600

    Extend NodeJS support.
    Extends NodeJS support to newer versions.

--- README.md
+++ README.md
@@ -3,11 +3,16 @@
 Predefined gulp tasks for Wordpress MVC (WPMVC).
 
 ## Requirements
-* node <= v0.12.0
-* npm <= v2.5.1
+* node >= v6.0.0
+* npm >= v2.3.0
+
+**NOTE:** If using *node* `v0.12.0` use release `v1.0.9`.
+
+## Troubleshooting
+If experiencing issues with sass and other modules, use [nvm](https://github.com/creationix/nvm) or [nvm for Windows](https://github.com/coreybutler/nvm-windows) to downgrade your version of **NodeJS**.
 
 ## License
 
 MIT license.
 
-2016 [10 Quality](http://www.10quality.com/).
+(c) 2017 [10 Quality](http://www.10quality.com/).
---
+++ tests/test.js
@@ -0,0 +1,49 @@
+/**
+ * Test unit.
+ * @version 1.1.0
+ */
+var assert = require('assert');
+var fs = require('fs');
+var gulp = require('gulp');
+var wpmvc = require('../lib/wpmvc');
+// Load package JSON as config file.
+var config = JSON.parse(fs.readFileSync('./package.json'));
+
+describe('wpmvc', function() {
+    // Prepare
+    wpmvc(gulp, config);
+    // Assert
+    it('Task "styles" exists.', function() {
+        assert.notEqual(undefined, gulp.task('styles')); 
+    });
+    it('Task "scrits" exists.', function() {
+        assert.notEqual(undefined, gulp.task('scrits')); 
+    });
+    it('Task "sass" exists.', function() {
+        assert.notEqual(undefined, gulp.task('sass')); 
+    });
+    it('Task "jsmin" exists.', function() {
+        assert.notEqual(undefined, gulp.task('jsmin')); 
+    });
+    it('Task "cssmin" exists.', function() {
+        assert.notEqual(undefined, gulp.task('cssmin')); 
+    });
+    it('Task "build-files" exists.', function() {
+        assert.notEqual(undefined, gulp.task('build-files')); 
+    });
+    it('Task "build-prezip" exists.', function() {
+        assert.notEqual(undefined, gulp.task('build-prezip')); 
+    });
+    it('Task "build-zip" exists.', function() {
+        assert.notEqual(undefined, gulp.task('build-zip')); 
+    });
+    it('Task "build-clean" exists.', function() {
+        assert.notEqual(undefined, gulp.task('build-clean')); 
+    });
+    it('Task "dev" exists.', function() {
+        assert.notEqual(undefined, gulp.task('dev')); 
+    });
+    it('Task "build" exists.', function() {
+        assert.notEqual(undefined, gulp.task('build')); 
+    });
+});
